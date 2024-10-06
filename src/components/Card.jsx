4b01f81f8672aea6e7cc33c5e534530267e5cb55
YolcuM <maryam.pana@gmail.com>
2024-10-06 23:51:01 +0300

    cardsNEw

---
+++ src/components/Card.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import styles from './Card.module.css';
+import { useState } from 'react';
+
+const Card = ({ call, price, currency, month, second, write, headerColor = '', mainColor = '', isTaller = false }) => {
+    
+    const [isSelected, setIsSelected] = useState(false);
+
+    const handleIsSelected = () => {
+        setIsSelected(!isSelected)
+    }
+    
+    return (
+        <div className={`${styles.card} ${isSelected ? styles.card_selected : ''}`}
+        style={{ height: isTaller ? '20rem' : '18rem' }}
+        onClick={handleIsSelected}>
+
+            <div className={styles.card_header} style={{ backgroundColor: headerColor }}>
+                {call}
+            </div>
+            <div className={styles.card_main} style={{ backgroundColor: mainColor }}>
+                <div className={styles.card_main_text_container}>
+                    <p className={styles.card_main_text_container_small_text}>{price}</p>
+                    <p className={styles.card_main_text_container_large_text}>{currency}</p>
+                    <p className={styles.card_main_text_container_small_text}>{month}</p>
+                </div>
+            </div>
+            <div className={styles.card_footer_light}>
+                {second}
+</div>
+            <div className={styles.card_footer_dark}>
+                {write}
+            </div>
+        </div>
+    );
+};
+
+export default Card;
---
+++ src/components/Cards.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import data from '../Data';
+import Card from './Card';
+import styles from './Cards.module.css';
+
+const Cards = () => {
+    return (
+        <div className={styles.cards_container}>
+        {data.map((info, index) => (
+            <Card key={info.call} {...info}
+            headerColor={index === 0 ? '#0b8fc2' : index === 1 ? '#33CC33' : index === 2 ? '#CC3333' : '#6666CC'}
+            mainColor={index === 0 ? '#26b8f1' : index === 1 ? '#63ec62' : index === 2 ? '#ee5656' : '#8a8ae8'}
+            isTaller={index === 2 ? true : false}
+            />
+        ))}
+        </div>
+    );
+};
+
+export default Cards;
